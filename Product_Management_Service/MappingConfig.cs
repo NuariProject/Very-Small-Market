28ef608cae997c82aa7220954951307a62b0f5b2
BaniPhuser <nuari.br@gmail.com>
2023-08-27 08:34:31 +0700

    Create Config automapper create depedency injection create connection string

---
+++ Product_Management_Service/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Product_Management_Service.Context;
+using Product_Management_Service.Models.DTO;
+
+namespace Product_Management_Service
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Category, CategoryCreatedDTO>().ReverseMap();
+            CreateMap<Category, CategoryUpdatedDTO>().ReverseMap();
+
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<ProductDTO, ProductCreatedDTO>().ReverseMap()
+                .ForMember(dest => dest.CategoryList, opt => opt.MapFrom(src => src.CategoryList))
+                .ReverseMap();
+            CreateMap<ProductDTO, ProductUpdatedDTO>().ReverseMap()
+                .ForMember(dest => dest.CategoryList, opt => opt.MapFrom(src => src.CategoryList))
+                .ReverseMap();
+        }
+    }
+}
--- Product_Management_Service/Program.cs
+++ Product_Management_Service/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Product_Management_Service;
+using Product_Management_Service.Context;
+using Product_Management_Service.Repository;
+using Product_Management_Service.Repository.IRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// create connection string
+builder.Services.AddDbContext<vsm_db_productContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("dbCon"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// create depedency injection
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 var app = builder.Build();
 
